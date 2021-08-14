005ca3bc867676168afa803836901892d7aa6a74
XDXGLBJDHZ <1426061489@qq.com>
2021-08-14 10:28:14 +0800

    2021/8/14

---
+++ ssh_learn/hibernate/hibernate_learn2/src/main/java/cn/itcast/hibernatetest/HibernateDemo.java
@@ -0,0 +1,42 @@
+package cn.itcast.hibernatetest;
+import cn.itcast.entity.User;
+import cn.itcast.utils.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ * @author ME08I
+ * @date 2021/8/13
+ */
+public class HibernateDemo {
+    public void testAdd() {
+//        第一步：加载hibernate核心配置文件
+        //到src下找到hibernate.cfg.xml文件
+        //hibernate中封装的对象
+//        Configuration cfg = new Configuration();
+//        cfg.configure();
+//        第二步：创建SessionFactory对象
+        //读取hibernate的核心配置文件
+        //在过程中，根据映射关系，在配置的数据库中创建表
+//        SessionFactory sessionFactory = cfg.buildSessionFactory();
+        SessionFactory sessionFactory= HibernateUtils.getSessionFactory();
+//        第三步：使用SessionFactory对象创建session对象（与Http中的session没有任何关系）
+        Session session = sessionFactory.openSession();
+//        第四步：开启事务（事物是操作过程中最基本的单元）
+        Transaction transaction = session.beginTransaction();
+//        第五步：具体操作逻辑，crud操作（除这一步有变化，其余步骤都是固定操作）
+        //添加功能
+        User user = new User();
+        user.setId(2013);
+        user.setUsername("YZdfhK");
+        user.setPassword("2203dfg41");
+        //调用session中的方法来实现添加
+        session.save(user);
+//        第六步：提交事物
+        transaction.commit();
+//        第七步：关闭资源
+        session.close();
+        sessionFactory.close();
+    }
+}
---
+++ ssh_learn/hibernate/hibernate_learn2/src/main/java/cn/itcast/utils/HibernateUtils.java
@@ -0,0 +1,25 @@
+package cn.itcast.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author ME08I
+ * @date 2021/8/13
+ */
+public class HibernateUtils {
+    //静态代码块中只执行一次，所以变量也必须是静态的
+    static Configuration cfg=null;
+    static SessionFactory sessionFactory=null;
+    //静态代码块实现
+    static {
+        //加载核心配置文件
+        cfg=new Configuration();
+        cfg.configure();
+        sessionFactory=cfg.buildSessionFactory();
+    }
+    //提供返回sessionFactory的方法
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+}
